44ff8eefb0097e1f993a24f407691198cd186703
kais Alkotamy <kais.alkotamy@gmail.com>
2024-03-09 05:32:37 +0400

    ex handling -> schema validation

--- src/exception/mongo-ex.filter.ts
+++ src/exception/mongo-ex.filter.ts
@@ -3,15 +3,19 @@
 import { ArgumentsHost, Catch, ConflictException, ExceptionFilter, HttpStatus, ValidationError } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { MongooseError } from 'mongoose';
+import { LoggerService } from 'src/logger/logger.service';
 
 @Catch(MongooseError)
 export class MongoExceptionFilter implements ExceptionFilter {
+    constructor(private readonly logger: LoggerService) { }
     catch(exception: any, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
         const message = exception.message;
 
+        // Log the exception
+        this.logger.error(message, exception.trace, exception.name);
         response
             .status(HttpStatus.BAD_REQUEST)
             .json({
